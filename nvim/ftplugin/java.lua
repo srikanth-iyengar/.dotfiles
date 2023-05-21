77feeefc853c517ce94231091f07b3b0d8ed42b2
Srikanth Iyengar <srikanth.k@spit.ac.in>
2023-05-21 14:30:07 +0530

    tmux Changed

--- nvim/ftplugin/java.lua
+++ nvim/ftplugin/java.lua
@@ -1,2 +1,7 @@
-require("lspconfig").jdtls.setup{}
-vim.cmd[[LspStart<CR>]]
+local server = require("user.lsp_servers")
+if server["java"] == nil then
+    require("lspconfig").jdtls.setup{}
+    vim.cmd[[LspStart<CR>]]
+    server["java"] = true
+end
+
